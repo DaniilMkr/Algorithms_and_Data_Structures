a7f132d7c9463c75fdcc2bf0c36e76c6f6171c23
Daniil Makarov <daniilmakarovmk@gmail.com>
2023-06-03 13:56:20 +0200

    1. Add IsUnique 2. Add MaxProductofTwoIntegers 3. Add Permutation

---
+++ src/main/java/com/daniil/MaxProductofTwoIntegers/MaxProductofTwoIntegers.java
@@ -0,0 +1,28 @@
+package com.daniil.MaxProductofTwoIntegers;
+
+
+import java.util.Arrays;
+
+public class MaxProductofTwoIntegers {
+    public static void main(String[] args) {
+        int[] nums = new int[]{30, 10, 20, 50, 60};
+        int [] result = maxProduct(nums);
+        System.out.println(Arrays.toString(result));
+    }
+
+
+    public static int[] maxProduct(int[] intArray) {
+        int maxOne = 0;
+        int maxTwo = 0;
+        for (int i = 0; i < intArray.length; i++) {
+            if (maxOne < intArray[i]) {
+                maxTwo = maxOne;
+                maxOne = intArray[i];
+            } else if (maxTwo < intArray[i]) {
+                maxTwo = intArray[i];
+
+            }
+        }
+        return new int[]{maxOne, maxTwo};
+    }
+}
